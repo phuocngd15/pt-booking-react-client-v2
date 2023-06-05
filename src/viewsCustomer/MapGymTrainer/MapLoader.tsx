5198fbc1ac9c9d0be4e31db6fedf83ff172f54a1
phuocngd15 <phuocngd15@gmail.com>
2023-06-05 23:13:37 +0700

    a litle map

--- src/viewsCustomer/MapGymTrainer/MapLoader.tsx
+++ src/viewsCustomer/MapGymTrainer/MapLoader.tsx
@@ -3,49 +3,57 @@ import { Loader } from '@googlemaps/js-api-loader';
 import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import { stylesSilver } from '@/viewsCustomer/MapGymTrainer/constant';
 
-const GoogleMap = () => {
-  const locations = [
-    {
-      position: { lat: 10.984571001127811, lng: 106.66716863206851 },
-      contentString: 'Location 1',
-      label: 'A',
-    },
-    {
-      position: { lat: 10.980915601259877, lng: 106.67333310905951 },
-      contentString: 'Location 2',
-      label: 'B',
-    },
-    // Add more locations as needed...
-  ];
-
+const GoogleMap = ({ userLocation }: { userLocation: any }) => {
   useEffect(() => {
+    if (!userLocation) return;
+
+    const locations = [
+      userLocation,
+      {
+        position: { lat: 10.984571001127811, lng: 106.66716863206851 },
+        contentString: 'Location 1',
+        label: 'A',
+      },
+      {
+        position: { lat: 10.980915601259877, lng: 106.67333310905951 },
+        contentString: 'Location 2',
+        label: 'B',
+      },
+      // Add more locations as needed...
+    ];
     const loadMap = () => {
       const center = locations[0].position;
       const defaultZoom = 15;
       const loader = new Loader({
-        apiKey: 'key',
+        apiKey: '',
         version: 'weekly',
       });
 
       loader.load().then((e) => {
+        console.log("window.google.maps",window.google.maps)
+        const { Map, InfoWindow, Marker } = window.google.maps;
+        //const { AdvancedMarkerElement, PinElement } = window.google.maps.Marker;
+
         console.log('e', e);
-        const map = new window.google.maps.Map(document.getElementById('map'), {
+        const map = new Map(document.getElementById('map'), {
           zoom: defaultZoom,
           center,
           streetViewControl: false,
           mapTypeControl: false,
         });
         map.setOptions({ styles: stylesSilver });
+
         const markers = [];
+
         locations.forEach((location) => {
           const { position, contentString, label } = location;
 
-          const infoWindow = new window.google.maps.InfoWindow({
+          const infoWindow = new InfoWindow({
             content: contentString,
             ariaLabel: label,
           });
 
-          const marker = new window.google.maps.Marker({
+          const marker = new Marker({
             position,
             label,
             map,
@@ -72,7 +80,8 @@ const GoogleMap = () => {
     };
 
     loadMap();
-  }, []);
+  }, [userLocation]);
+  console.log("window.google.maps after load",window.google.maps)
 
   return <div id="map" style={{ height: '400px' }} />;
 };
--- src/viewsCustomer/MapGymTrainer/index.tsx
+++ src/viewsCustomer/MapGymTrainer/index.tsx
@@ -2,34 +2,40 @@ import { Col, Row } from 'antd';
 import React, { useEffect, useState } from 'react';
 import MapLoader from '@/viewsCustomer/MapGymTrainer/MapLoader';
 import { getLocation } from '@/viewsCustomer/MapGymTrainer/helper';
-
+import mapdemo from './mapdeco.png';
+import styles from './MapGym.module.css'
 export default function MapGymTrainer() {
-  // const [userLocation, setUserLocation] = useState<any>();
-  // useEffect(() => {
-  //   const handleLocationError = (error) => {
-  //     console.error('Error getting current location:', error);
-  //   };
-  //
-  //   getLocation()
-  //     .then((location: any) => {
-  //       const userlocation = {
-  //         position: { lat: location.latitude, lng: location.longitude },
-  //         contentString: 'You',
-  //         label: 'You',
-  //       };
-  //       console.log('userlocation', userlocation);
-  //       setUserLocation(userlocation);
-  //     })
-  //     .catch((error) => handleLocationError(error));
-  // }, []);
+  const [userLocation, setUserLocation] = useState<any>();
+  useEffect(() => {
+    const handleLocationError = (error) => {
+      console.error('Error getting current location:', error);
+    };
+
+    getLocation()
+      .then((location: any) => {
+        const userlocation = {
+          position: { lat: location.latitude, lng: location.longitude },
+          contentString: 'You',
+          label: 'You',
+        };
+        console.log('userlocation', userlocation);
+        setUserLocation(userlocation);
+      })
+      .catch((error) => handleLocationError(error));
+  }, []);
   return (
     <div>
       <Row gutter={[24, 24]}>
         <Col span={4}>
           <div>FIND YOUR GYM</div>
         </Col>
-        <Col span={20} />
-        <MapLoader />
+        <Col span={20}>
+
+            <div className={styles.mapcontainer}>
+                {/*<img src={mapdemo}/>*/}
+                <MapLoader userLocation={userLocation} />
+            </div>
+        </Col>
       </Row>
     </div>
   );
